95fc29c7225b037fc1de84c539db5a2b0e03c227
Raphaël <raphael.bergoin@etu.u-cergy.fr>
2018-01-13 15:18:07 +0100

    Programme initial

---
+++ D-Late/src/exception/TerminusException.java
@@ -0,0 +1,11 @@
+package exception;
+
+/**
+ * @author tliu@u-cergy.fr
+ */
+public class TerminusException extends Exception {
+
+	
+	private static final long serialVersionUID = -955832516208577103L;
+
+}
